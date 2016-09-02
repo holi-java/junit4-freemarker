c4a5fd50063d7b59dbcda5261df8e2032270dd0a
selonj <selonj@126.com>
2016-09-02 18:07:48 +0800

    separate bugs from acceptance tests

--- freemarker-test/src/test/java/com/holi/junit/FreeMarkerRunnerAcceptanceTest.java
+++ freemarker-test/src/test/java/com/holi/junit/FreeMarkerRunnerAcceptanceTest.java
@@ -121,38 +121,7 @@ public class FreeMarkerRunnerAcceptanceTest {
     result.assertAllTestsPassed();
   }
 
-  @Test public void fixBugIfMacroAndAssignmentDefinedBeforeTest() throws Throwable {
-    TestResult result = test(
-  /**/   "<#assign foo='bar'>"
-  /**/ + "<#macro _></#macro>"
-  /**/ + "<@test></@test>"
-    );
 
-    result.hasRanTests(1);
-    result.assertAllTestsPassed();
-  }
-
-  @Test public void fixBugAssertCannotUseGroupsBuiltInCorrectly() throws Throwable {
-    TestResult result = test(
-    /**/   "<#assign foo='*'?matches('(.)')>"
-    /**/ + "<@assert expected='*' actual=foo?groups[1]/>"
-    /**/ + "<@assert expected='*' actual=foo?groups[1]/>"
-    );
-
-    result.hasRanTests(1);
-    result.assertAllTestsPassed();
-  }
-
-  @Test public void fixBugCurrentEnvironmentWasNullWhenRunTestBlock() throws Throwable {
-    TestResult result = test(
-    /**/   "<@test name='foo'>\n"
-    /**/ + "  <@assert expected=[1] actual=[1]?sort/>\n"
-    /**/ + "</@test>\n"
-    );
-
-    result.hasRanTests(1);
-    result.assertAllTestsPassed();
-  }
 
   private TestResult test(String snippet) throws Throwable {
     return TestResult.test(scriptName, snippet);
---
+++ freemarker-test/src/test/java/com/holi/junit/FreeMarkerRunnerBugsTest.java
@@ -0,0 +1,52 @@
+package com.holi.junit;
+
+import com.holi.junit.utils.TestResult;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+
+/**
+ * Created by selonj on 16-8-29.
+ */
+public class FreeMarkerRunnerBugsTest {
+
+  public static final String scriptName = "test.ftl";
+  public static final String test = "test";
+
+  @Test public void fixBugIfMacroAndAssignmentDefinedBeforeTest() throws Throwable {
+    TestResult result = test(
+  /**/   "<#assign foo='bar'>"
+  /**/ + "<#macro _></#macro>"
+  /**/ + "<@test></@test>"
+    );
+
+    result.hasRanTests(1);
+    result.assertAllTestsPassed();
+  }
+
+  @Test public void fixBugAssertCannotUseGroupsBuiltInCorrectly() throws Throwable {
+    TestResult result = test(
+    /**/   "<#assign foo='*'?matches('(.)')>"
+    /**/ + "<@assert expected='*' actual=foo?groups[1]/>"
+    /**/ + "<@assert expected='*' actual=foo?groups[1]/>"
+    );
+
+    result.hasRanTests(1);
+    result.assertAllTestsPassed();
+  }
+
+  @Test public void fixBugCurrentEnvironmentWasNullWhenRunTestBlock() throws Throwable {
+    TestResult result = test(
+    /**/   "<@test name='foo'>\n"
+    /**/ + "  <@assert expected=[1] actual=[1]?sort/>\n"
+    /**/ + "</@test>\n"
+    );
+
+    result.hasRanTests(1);
+    result.assertAllTestsPassed();
+  }
+
+  private TestResult test(String snippet) throws Throwable {
+    return TestResult.test(scriptName, snippet);
+  }
+}
